7b762ceb6ba66838ead2fc2a8d7a1dadd415552f
StivenRMS <79185721+StivenRMS@users.noreply.github.com>
2021-02-19 21:03:02 -0600

    Add files via upload

---
+++ Ejercicio 3/Ejer.3_Obrero/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ejer._3_Obrero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horas2;
+            string horas1;
+            int horas, horasex;
+            Console.Write("Ingrese las horas que trabajó:");
+            horas1 = Console.ReadLine();
+            Console.WriteLine(" ");
+            horas2 = Convert.ToInt32(horas1);
+
+            if (horas2 <= 40)
+            {
+                horas = (horas2 * 16);
+                Console.WriteLine("Su Salario es de: Q" + horas);}
+            else {
+               horasex=  horas2 - 40;
+                horas2 = (40 * 16);
+                horas = ((horasex * 20)+ horas2);
+                Console.WriteLine("Por trabajar " + horasex + " horas extras ");
+                Console.WriteLine(" ");
+                Console.WriteLine("Su salario es de: Q" + horas);
+
+
+            }
+        }
+    }
+}
