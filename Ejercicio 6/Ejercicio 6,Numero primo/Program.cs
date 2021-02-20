237fe5dce03e9004ab19d4ca180b3c2d24c1bbe6
StivenRMS <79185721+StivenRMS@users.noreply.github.com>
2021-02-19 21:05:45 -0600

    Add files via upload

---
+++ Ejercicio 6/Ejercicio 6,Numero primo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ejercicio_6_Numero_primo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, num;
+            Console.WriteLine("Ingrese el Numero que desea saber si es primo: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < (num + 1); i++)
+            {
+                if (num % i == 0)
+                {
+                    a++;
+                }
+            }
+            if (a != 2)
+            {
+                Console.WriteLine(num + " No es un número primo");
+            }
+            else
+            {
+                Console.WriteLine(num + " Si es un número primo");
+            }
+            Console.ReadLine();
+        }
+    }
+    }
+
