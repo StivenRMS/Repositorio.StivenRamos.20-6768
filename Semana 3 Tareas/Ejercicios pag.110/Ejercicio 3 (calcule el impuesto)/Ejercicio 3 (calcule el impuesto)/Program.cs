252f0cac67204d6c4e8d03cd9ac1231ddb77a999
StivenRMS <79185721+StivenRMS@users.noreply.github.com>
2021-02-26 00:15:47 -0600

    Add files via upload

---
+++ Semana 3 Tareas/Ejercicios pag.110/Ejercicio 3 (calcule el impuesto)/Ejercicio 3 (calcule el impuesto)/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Ejercicio_3__calcule_el_impuesto_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                int num;
+
+                Console.WriteLine("Medicina......(1)");
+                Console.WriteLine("OTRO PRODUCTO.(2)");
+                Console.WriteLine("");
+                Console.Write("INGRESE EL NUMERO DE SU OPCION: ");
+                num = Convert.ToInt32(Console.ReadLine());
+
+
+                switch (num)
+                {
+                    case 1:
+                        //Si es medicina
+                        int precio;
+                        Console.Write("Ingrese el precio total de la medicina: ");
+                        precio = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(" ");
+                        Console.WriteLine("Su impuesto es del 0%");
+                        Console.WriteLine(" ");
+                        Console.WriteLine("Su Producto esta libre de pagar impuestos ");
+                        break;
+                    case 2:
+                        int precio1;
+                        double total, total1, iva = 1.12;
+                        Console.Write("Ingrese el precio total del producto: ");
+                        
+                        precio1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(" ");
+                        Console.WriteLine("Su impuesto es del 12%");
+                        Console.WriteLine(" ");
+                        total = Math.Round((precio1 / iva),2);
+                        total1 = Math.Round((precio1 - total),2);
+                        Console.WriteLine("Precio sin Impuesto: "+ total);
+                        Console.WriteLine("El total del impuesto es de: " + total1);
+                        break;
+                    default:
+                        Console.WriteLine(" ");
+                        Console.WriteLine("Ingrese una de las dos opciones!");
+                        break;
+
+
+
+
+                }
+                
+            }
+            
+            catch (Exception error) { Console.WriteLine("¡EROR! " + error.Message); }
+
+
+
+
+
+        }
+    }
+    
+}
---
+++ Semana 3 Tareas/Ejercicios pag.110/Ejercicio 4 (Día de semana)/Ejercicio 4 (Día de semana)/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Ejercicio_4__Día_de_semana_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                int num;
+                string dia, num1;
+                Console.WriteLine("ESCRIBA UN NUMERO DEL 1 AL 7: ");
+                num1=Console.ReadLine();
+                num = Convert.ToInt32(num1);
+                Console.WriteLine(" ");
+                
+
+                switch (num)
+                {
+                    case 1:
+                        dia = "LUNES";
+                        Console.Write("El dia según el numero es: " + dia);
+                        break;
+                    case 2:
+                        dia = "MARTES";
+                        Console.Write("El dia según el numero es: " + dia);
+                        break;
+                    case 3:
+                        dia = "MIERCOLES";
+                        Console.Write("El dia según el numero es: " + dia);
+                        break;
+                    case 4:
+                        dia = "JUEVES";
+                        Console.Write("El dia según el numero es: " + dia);
+                        break;
+                    case 5:
+                        dia = "VIERNES";
+                        Console.Write("El dia según el numero es: " + dia);
+                        break;
+                    case 6:
+                        dia = "SABADO";
+                        Console.Write("El dia según el numero es: " + dia);
+                        break;
+                    case 7:
+                        dia = "DOMINGO";
+                        Console.Write("El dia según el numero es: " + dia);
+                        break;
+                    default:
+                        Console.WriteLine(" ");
+                        Console.WriteLine("Número no disponible...");
+                        break;
+
+
+
+
+                }
+                
+            }
+            catch (Exception error) { Console.WriteLine("¡EROR! " + error.Message); }
+
+
+
+
+
+        }
+    }
+    
+}
