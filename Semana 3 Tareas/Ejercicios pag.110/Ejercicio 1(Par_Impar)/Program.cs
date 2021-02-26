c9451f7e00424bd2f8dd29d844ac4cd5b2f98d90
StivenRMS <79185721+StivenRMS@users.noreply.github.com>
2021-02-26 00:11:00 -0600

    Add files via upload

---
+++ Semana 3 Tareas/Ejercicios pag.110/Ejercicio 1(Par_Impar)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ejercicio_1_Par_Impar_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double n1;
+                Console.WriteLine("Ingrese el número: ");
+                n1 = double.Parse(Console.ReadLine());
+
+
+                if (n1 % 2 == 0)
+                {
+                    Console.WriteLine("El número " + n1 + " es par");
+
+                }
+                else { Console.WriteLine("El número " + n1 + " es impar"); }
+            }catch (Exception error)
+            {
+                Console.WriteLine($"Hay un error {error.Message}");
+            }
+
+        }
+    }
+}
---
+++ Semana 3 Tareas/Ejercicios pag.110/Ejercicio 2.(grados a radianes)/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Ejercicio_2._grados_a_radianes_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                int num;
+
+                Console.WriteLine("DE GRADOS A RADIANES (1)");
+                Console.WriteLine("DE RADIANES A GRADOS (2)");
+                Console.WriteLine("");
+                Console.Write("INGRESE EL NUMERO DE SU OPCION: ");
+                num = Convert.ToInt32(Console.ReadLine());
+           
+
+            switch (num) 
+            {
+              case 1: 
+                     //Si es de grado a radian
+                     double grados, radianes;
+                     string val;
+
+                      Console.Write("Dame la cantidad de grados: ");
+                      val = Console.ReadLine();
+                      grados = Convert.ToDouble(val);
+                      radianes = Math.Round(grados * (Math.PI / 180), 6);
+                      Console.WriteLine(" ");
+                      Console.WriteLine("El resultado es: "+radianes+" Radianes");
+                    break;
+              case 2:
+               double grados1, radianes1;
+               string val1;
+
+               Console.Write("Dame la cantidad de Radianes: ");
+               val1 = Console.ReadLine();
+               radianes1 = Convert.ToDouble(val1);
+               grados1 = Math.Round(((radianes1 * 180)/Math.PI), 6);
+               Console.WriteLine("El resultado es: "+grados1+"°");
+                    break;
+                default:
+                        Console.WriteLine(" ");
+                        Console.WriteLine("Ingrese una de las dos opciones!");
+                    break;
+
+              
+
+
+        }
+
+            }
+            catch (Exception error) { Console.WriteLine("¡EROR! " + error.Message); }
+
+
+
+
+
+        }
+    }
+}
