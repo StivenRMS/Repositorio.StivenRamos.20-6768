7e85043e2673d3620a40b01c63e8b5e93202203f
StivenRMS <79185721+StivenRMS@users.noreply.github.com>
2021-02-26 00:19:43 -0600

    Add files via upload

---
+++ Semana 3 Tareas/Ejercicios pag.146/Ejecicio 1(tabla de multiplicar)/Ejecicio 1(tabla de multiplicar)/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+namespace Ejecicio_1_tabla_de_multiplicar_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int num, num1;
+                Console.WriteLine("Introdusca el número que desea saber la tabla: ");
+                num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("La tabla es la siguiente: ");
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    num1 = num * i;
+                    Console.WriteLine(num + " x " + i + " = " + num1);
+
+                }
+            }catch (Exception error) { Console.WriteLine("¡EROR! " + error.Message); }
+
+
+        }
+    }
+}
---
+++ Semana 3 Tareas/Ejercicios pag.146/Ejercicio 2 (potencia)/Ejercicio 2 (potencia)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ejercicio_2__potencia_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int num,num1= 1, pot;
+                Console.Write("Introduzca el número: ");
+                num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(" ");
+                Console.Write("Introduzca la potencia: ");
+                pot = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(" ");
+
+                for (int i = 1; i <= pot; i++)
+                {
+                    num1 = num1 * num;
+                    
+
+                }
+                Console.WriteLine(" ");
+                Console.Write("...........................El resultado es: " + num1 );
+                Console.WriteLine(" ");
+
+            }
+            catch (Exception error) { Console.WriteLine("¡EROR! " + error.Message); }
+
+        }
+    }
+}
