bbd92329528e9b565c71055d5d2823f3355e3062
StivenRMS <79185721+StivenRMS@users.noreply.github.com>
2021-02-26 00:20:44 -0600

    Add files via upload

---
+++ Semana 3 Tareas/Ejercicios pag.146/Ejercicio 4 (Buscador numero primos)/Ejercicio 4 (Buscador numero primos)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ejercicio_4__Buscador_numero_primos_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("LOS NUMEROS PRIMOS QUE HAY DEL 1-1000: ");
+                int num =2, cont =0;
+                while (num <= 1000)
+                {
+                    for (int i = 1; i <= num; i++)
+                    {
+                        if (num % i == 0)
+                        {
+                            cont++;
+                        }
+                    }
+                    if (cont == 2)
+                    {
+                        Console.WriteLine("El número  " + num + " Sí es primo");
+                    }
+                    cont = 0;
+                    num++;
+                }
+            }
+            catch (Exception error) { Console.WriteLine("¡EROR! " + error.Message); }
+
+        }
+    }
+}
---
+++ Semana 3 Tareas/Ejercicios pag.146/Ejercicio 5 (Promedio de edad)/Ejercicio 5 (Promedio de edad)/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Ejercicio_5__Promedio_de_edad_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int nump;
+                int may = 0, men = 0;
+                Console.Write("Cantidades de Edades: ");
+                nump = Convert.ToInt32(Console.ReadLine());
+                int[] edades = new int[nump + 1];
+                int i;
+
+                for (i = 1; i <= nump; i++)
+                {
+                    Console.Write("Cual es la Edad de la Persona: ");
+                    edades[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                may = edades[1];
+                men = edades[1];
+                for (i = 1; i <= nump; i++)
+                {
+                    if (edades[i] > may)
+                    {
+                        may = edades[i];
+                    }
+                    else if (edades[i] < men)
+                    {
+                        men = edades[i];
+                    }
+                }
+
+                Console.WriteLine("La edad mayor es: " + may);
+                Console.WriteLine("La edad menor es: " + men);
+                Console.WriteLine(" ");
+            }catch (Exception error) { Console.WriteLine("¡EROR! " + error.Message); }
+
+        }
+    }
+}
