920169e36b711d4dfdf1f1555325d6f2d31f7fa8
StivenRMS <79185721+StivenRMS@users.noreply.github.com>
2021-02-26 00:24:04 -0600

    Add files via upload

---
+++ Semana 3 Tareas/Factorial de un Numero/Factorial de un Numero/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Factorial_de_un_Numero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int num, fact = 1;
+                Console.Write("Ingrese un Número:");
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num < 0)
+                {
+                    fact = 0;
+                }
+                else if (num == 0)
+                {
+                    fact = 1;
+                }
+                else
+                {
+                    for (int i = 1; i <= num; i++)
+                    {
+                        fact = fact * i;
+                    }
+                }
+                Console.WriteLine("||||||||||||||||||||||||||||||||||||||||||||||||||");
+                Console.WriteLine("El factorial del numero " + num + " es: " + fact);
+            }catch (Exception error) { Console.WriteLine("¡EROR! " + error.Message); }
+
+        }
+
+    }
+}
---
+++ Semana 3 Tareas/Serie de un Numero/Serie de un Numero/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Serie_de_un_Numero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+				Console.Write("Ingrese el numero: ");
+				int cantidad = int.Parse(Console.ReadLine());
+				long a = 0;
+				long b = 1;
+				long aux = 1;
+
+
+				for (int i = 0; i <= cantidad; i++)
+				{
+					if (i == 0)
+					{
+						Console.Write(a + ", ");
+					}
+					else
+					{
+						aux = a;
+						a = b;
+						b = aux + a;
+						Console.Write(a + ", ");
+					}
+				}
+				Console.WriteLine();
+			}catch (Exception error) { Console.WriteLine("¡EROR! " + error.Message); }
+
+		}
+	}
+}
