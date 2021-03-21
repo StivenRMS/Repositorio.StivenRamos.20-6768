d9c80dec5988c8dcdcdfbbba7615ed0af70b2f21
StivenRMS <79185721+StivenRMS@users.noreply.github.com>
2021-03-20 23:38:57 -0600

    Add files via upload

---
+++ Ejercicios EXPLICADOS/Ejercicio 1 Explicar/Ejercicio 2 Explicar/Ejercicio 2 Explicar/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace ejercicios__clase_1_para_parcial_2
+{
+    class Program
+    {
+
+        public static void dibuja(int cantidad)
+        {
+            int columnaT = 50;
+            int filaT = 100;
+            int IntervF = 2;
+            int IntervC = 2;
+            int F, C, A;
+            for (A = 1; A <= cantidad; A++)
+            {
+                for (F = IntervC; F <= filaT; F++)
+                {
+                    Console.SetCursorPosition(F, columnaT); Console.Write("?");
+                    Console.SetCursorPosition(F, IntervF); Console.Write("?");
+                }
+                for (C = IntervF; C <= columnaT; C++)
+                {
+                    Console.SetCursorPosition(IntervC, C); Console.Write("X");
+                    Console.SetCursorPosition(filaT, C); Console.Write("X");
+                }
+                IntervF = IntervF + 1;
+                columnaT = columnaT - 1;
+                IntervC = IntervC + 1;
+                filaT = filaT - 1;
+            }
+        }
+        static void Main(string[] args)
+        {
+            for (int tam = 1; tam < 25; tam++)
+            {
+                dibuja(tam);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ Ejercicios EXPLICADOS/Ejercicio 1 Explicar/Ejercicio 3 Explicar/Ejercicio 3 Explicar/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace RandomToken
+{
+    class Program
+    {
+        public static string Respuesta(int min, int max)
+        {
+            Random Res = new Random();
+            int Mayus = Convert.ToInt32(Res.Next(65, 90));
+            string RespF = "";
+            if (((Mayus > min) & (Mayus < max)))
+            {
+                RespF = Convert.ToString((char)Mayus);
+
+            }
+
+
+            return RespF;
+        }
+        static void Main(string[] args)
+        {
+            byte Cantidad, R;
+
+            string LeerCant;
+            Console.Write("Numero de caracteres deseados para su token: ");
+            LeerCant = Console.ReadLine();
+            Cantidad = byte.Parse(LeerCant);
+
+
+            string Respuesta = "";
+            for (R = 1; R <= Cantidad; R++)
+            {
+                Respuesta = Respuesta + Program.Respuesta(65, 90);
+            }
+            Console.WriteLine($"Su token es= {Respuesta}");
+
+        }
+    }
+}
